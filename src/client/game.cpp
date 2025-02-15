439c8f8aaba47ebe5919d840f734df0935ec0ec6
Gabri <73263831+Methro@users.noreply.github.com>
2025-02-15 18:03:52 -0500

    ¡ !

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -2606,7 +2606,6 @@ void Game::handleClientEvent_PlayerDamage(ClientEvent *event, CameraOrientation 
 		f32 hp_max = player->getCAO() ?
 			player->getCAO()->getProperties().hp_max : PLAYER_MAX_HP_DEFAULT;
 		f32 damage_ratio = event->player_damage.amount / hp_max;
-		
 
 		/* Disable damage flash
 		 runData.damage_flash += 95.0f + 64.f * damage_ratio;
